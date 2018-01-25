bdab7aa308e2bb4db2482dffb1140968cc5a6572
Luke Pinner <lpinner@users.noreply.github.com>
2018-01-25 14:54:39 +1100

    Implement a date range parameter with custom widget

--- src/datacube_query/parameters.py
+++ src/datacube_query/parameters.py
@@ -1,34 +1,32 @@
-from processing.core.parameters import Parameter
+from qgis.core import (QgsProcessingParameterString,)
 
-# noinspection PyUnresolvedReferences
-# pylint: disable=unused-import
-from .widgets import WrapperDateRange
+class ParameterDateRange(QgsProcessingParameterString):
+    """ Date range parameter that returns a '|' separated date range string in 'yyyy-MM-dd|yyy-MM-dd' format.
+        e.g. 2017-01-01|2017-12-31
 
-
-class ParameterDateRange(Parameter):
-    """  """
-    # TODO
-    def __init__(self, name, description, default=None, optional=False):
-        wrapper = {'widget_wrapper': 'widgets.WrapperDateRange'}
-        Parameter.__init__(name, description, default, optional) # Q3+, metadata=wrapper)
+        Null values are represented as empty strings,
+        e.g. '|', '2017-01-01|' or '|2017-12-31'
+    """
+    # TODO - validate start <= end
+    def __init__(self, name, description, *args, **kwargs):
+        wrapper = {'widget_wrapper': 'datacube_query.widgets.WrapperDateRange'}
+        QgsProcessingParameterString.__init__(self, name, description, *args, **kwargs)
         self.setMetadata(wrapper)
 
-    # noinspection PyMethodMayBeStatic
-    # pylint: disable=no-self-use
     def type(self):
         return 'date_range'
 
 
-class ParameterProducts(Parameter):
+class ParameterProduct(QgsProcessingParameterString):
     """  """
     # TODO
-    def __init__(self, name, description, default=None, optional=False):
-        wrapper = {'widget_wrapper': 'widgets.WrapperDateRange'}
-        Parameter.__init__(name, description, default, optional) # Q3+, metadata=wrapper)
+    def __init__(self, name, description, *args, **kwargs):
+        wrapper = {'widget_wrapper': 'datacube_query.widgets.WrapperProducts'}
+        QgsProcessingParameterString.__init__(self, name, description, *args, ** kwargs)
         self.setMetadata(wrapper)
 
     # noinspection PyMethodMayBeStatic
     # pylint: disable=no-self-use
     def type(self):
-        return 'products_measurements'
+        return 'product_measurements'
 
--- src/datacube_query/widgets.py
+++ src/datacube_query/widgets.py
@@ -6,20 +6,65 @@ from qgis.PyQt import uic
 _ui_path = os.path.join(os.path.dirname(__file__), 'ui')
 
 
-WIDGET, BASE = uic.loadUiType(
+WIDGET_DATE_RANGE, BASE_DATE_RANGE = uic.loadUiType(
     os.path.join(_ui_path, 'widget_daterange.ui'))
 
+WIDGET_PRODUCTS, BASE_PRODUCTS = uic.loadUiType(
+    os.path.join(_ui_path, 'widget_product.ui'))
 
-class WidgetDateRange(BASE, WIDGET):
 
-    def __init__(self, options):
-        super(DateRangeWidget, self).__init__(None)
+class WidgetDateRange(BASE_DATE_RANGE, WIDGET_DATE_RANGE):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
         self.setupUi(self)
 
+        self._dateformat = 'yyyy-MM-dd'
+
+        self._start = self.date_start.dateTime().toString(self._dateformat)
+        self._end = self.date_end.dateTime().toString(self._dateformat)
+
+        self.date_start.valueChanged.connect(self.update_start)
+        self.date_end.valueChanged.connect(self.update_end)
+
+    def update_start(self, qdatetime):
+        self._start = qdatetime.toString(self._dateformat)
+
+    def update_end(self, qdatetime):
+        self._end = qdatetime.toString(self._dateformat)
+
     def value(self):
-        return self.date_start.toPlainText(), self.date_end.toPlainText()
+        return '|'.join([self._start, self._end])
 
 
 class WrapperDateRange(WidgetWrapper):
-    def _panel(self, options):
-        return WidgetDateRange(options)
+
+    def _panel(self, *args, **kwargs):
+        return WidgetDateRange(*args, **kwargs)
+
+    def createWidget(self, *args, **kwargs):
+        return self._panel(*args, **kwargs)
+
+    def value(self):
+        return self.widget.value()
+
+
+
+class WidgetProducts(BASE_PRODUCTS, WIDGET_PRODUCTS):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.setupUi(self)
+
+    def value(self):
+        return "PRODUCT", "MEASUREMENTS"
+
+
+class WrapperProducts(WidgetWrapper):
+
+    def _panel(self, *args, **kwargs):
+        print(self.__class__)
+        return WidgetProducts(*args, **kwargs)
+
+    def createWidget(self, *args, **kwargs):
+        return self._panel(*args, **kwargs)
