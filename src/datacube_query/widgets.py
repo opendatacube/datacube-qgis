536c591935d2df4cd3f4ac6cf0d36f2f41e18de1
Luke Pinner <lpinner@users.noreply.github.com>
2018-02-01 15:37:26 +1100

    Implement custom QTreeWidget for products and measurements

--- src/datacube_query/widgets.py
+++ src/datacube_query/widgets.py
@@ -1,16 +1,19 @@
+from collections import defaultdict
 import os
 import json
 
 from processing.gui.wrappers import WidgetWrapper
+
 from qgis.PyQt import uic
+from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtWidgets import QTreeWidgetItem, QTreeWidgetItemIterator
 
 _ui_path = os.path.join(os.path.dirname(__file__), 'ui')
-
 
 WIDGET_DATE_RANGE, BASE_DATE_RANGE = uic.loadUiType(
     os.path.join(_ui_path, 'widget_daterange.ui'))
 
-WIDGET_PRODUCTS, BASE_PRODUCTS = uic.loadUiType(
+WIDGET_PRODUCT, BASE_PRODUCT = uic.loadUiType(
     os.path.join(_ui_path, 'widget_product.ui'))
 
 
@@ -47,42 +50,70 @@ class WrapperDateRange(WidgetWrapper):
         return self.widget.value()
 
 
-class WidgetProducts(BASE_PRODUCTS, WIDGET_PRODUCTS):
+class WidgetProducts(BASE_PRODUCT, WIDGET_PRODUCT):
 
     def __init__(self, data=None, *args, **kwargs):
         super().__init__()
         self.setupUi(self)
 
         self._data = None
-        self._product = ''
-        self._measurements = []
-
         self.set_data(data)
 
-        self.cbx_products.currentTextChanged.connect(self.update_measurements)
+        # In case we need to implement single product selections only
+        # self.tree_products.itemClicked.connect(self.check_item)
 
-    def value(self):
-        return json.dumps([self._product, self._measurements])
+    def check_item(self, item, column=0):
 
-    def set_data(self, data):
-        self.cbx_products.clear()
-        self.cbx_measurements.clear()
+        """ Stub for implementing single product selections only"""
+        # Get items, check item != clicked item, uncheck non-clicked parents
+        pass
 
+    def get_checked(self):
+        return self.get_items(flags=QTreeWidgetItemIterator.Checked)
+
+    def get_items(self, item=None, flags=QTreeWidgetItemIterator.All):
+        """ Returns an iterator over the tree items. """
+
+        if item is None:
+            item = self.tree_products
+
+        twit = QTreeWidgetItemIterator(item, flags)
+
+        while twit.value() is not None:
+            yield twit.value()
+            twit += 1
+
+    def set_data(self, data=None):
+        self.tree_products.clear()
         self._data = data if data else {}
 
-        for product in self._data:
-            self.cbx_product.addItem(product)
+        for product, measurements in self._data.items():
+            parent = QTreeWidgetItem(self.tree_products)
+            parent.setText(0, product)
+            parent.setFlags(parent.flags() | Qt.ItemIsTristate | Qt.ItemIsUserCheckable)
 
-        self.update_measurements()
+            for measurement in measurements:
+                child = QTreeWidgetItem(parent)
+                child.setFlags(child.flags() | Qt.ItemIsUserCheckable)
+                child.setText(0, measurement)
+                child.setCheckState(0, Qt.Unchecked)
 
-    def update_measurements(self):
-        self.cbx_measurements.clear()
-        self._product = self.cbx_products.currentText()
+    def get_data(self):
+        """ Return checked """
+        data = defaultdict(list)
 
-        measurements = self._data.get(self._product, [])
-        for measurement in measurements:
-            self.cbx_measurements.addItem(measurement)
+        for item in self.get_checked():
+            value = item.text(0)
+            parent = item.parent()
+            if parent is None:
+                _ =data[value]
+            else:
+                data[parent.text(0)].append(value)
 
+        return data
+
+    def value(self):
+        return json.dumps(self.get_data())
 
 
 class WrapperProducts(WidgetWrapper):
@@ -90,8 +121,43 @@ class WrapperProducts(WidgetWrapper):
     def createWidget(self, *args, **kwargs):
         return WidgetProducts(*args, **kwargs)
 
+    def setValue(self, data):
+        self.widget.set_data(data)
+
     def value(self):
         return self.widget.value()
 
-    def set_data(self, data):
-        self.widget.set_data(data)
+# # Stub for alternative TreeView widget using Model-View-Controller (MVC) design pattern
+# WIDGET_PRODUCTS, BASE_PRODUCTS = uic.loadUiType(
+#     os.path.join(_ui_path, 'widget_products.ui')) # This is a QTreeView, not a QTreeWidget
+#
+# class WidgetProducts(BASE_PRODUCTS, WIDGET_PRODUCTS):
+#
+#     def __init__(self, data=None, *args, **kwargs):
+#         super().__init__()
+#         self.setupUi(self)
+#
+#         self._data = None
+
+#         self._model = QStandardItemModel()
+#
+#         self.set_data(data)
+#
+#     def set_data(self, data=None):
+#         self._data = data if data else {}
+#         self._model = QStandardItemModel()
+#         self.tree_products.setModel(self._model)
+#         root = self._model.invisibleRootItem()
+#
+#         for product, measurements in self._data.items():
+#             parent = QStandardItem(product)
+#             parent.setFlags(parent.flags() | Qt.ItemIsTristate | Qt.ItemIsUserCheckable)
+#             parent.setCheckState(Qt.Unchecked)
+#             root.appendRow(parent)
+#
+#             for measurement in measurements:
+#                 child = QStandardItem(measurement)
+#                 child.setFlags(child.flags() | Qt.ItemIsUserCheckable)
+#                 #child.setText(0, measurement)
+#                 child.setCheckState(Qt.Unchecked)
+#                 parent.appendRow(child)
