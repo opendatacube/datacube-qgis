4d6327b929a22f662082a08b6617758b7de7d2f7
Luke Pinner <lpinner@users.noreply.github.com>
2018-02-08 15:11:03 +1100

    Segfault whackamole

--- src/datacube_query/defaults.py
+++ src/datacube_query/defaults.py
@@ -1,7 +1,37 @@
-from rasterio.enums import Resampling, Compression
+# TODO Disabling rasterio enums (for now).
+# Having this enabled segfaults QGIS 2.99dev (3.0) when the "Select CRS" button
+# in _any_ processing alg. dialog (even core) is clicked.
+# Might be related to rasterio 0.36. Upgrading to 1.0a(n) fixes the segfault
+# However, could be rasterio's use of enum.Enum which I've separately found to segfault QGIS...
 
+# from rasterio.enums import Resampling, Compression
+#
+# GTIFF_COMPRESSION = [c.value for c in Compression]
+# GTIFF_OVR_RESAMPLING = {r.name: r.value for r in Resampling}
 
-GTIFF_COMPRESSION = [c.value for c in Compression]
+GTIFF_COMPRESSION = [
+    'JPEG',
+    'LZW',
+    'PACKBITS',
+    'DEFLATE',
+    'LZMA',
+    'NONE'
+]
+
+GTIFF_OVR_RESAMPLING = {
+    'nearest': 0,
+    'bilinear': 1,
+    'cubic': 2,
+    'cubic_spline': 3,
+    'lanczos': 4,
+    'average': 5,
+    'mode': 6,
+    'gauss': 7,
+    'max': 8,
+    'min': 9,
+    'med': 10,
+    'q1': 11,
+    'q3': 12}
 
 GTIFF_DEFAULTS = {"driver": "GTiff",
                   "interleave": "band", "tiled": True,
@@ -14,10 +44,7 @@ GTIFF_DEFAULTS = {"driver": "GTiff",
                   'photometric': 'RGBA',
                   }
 
-GTIFF_OVR_RESAMPLING = {r.name: r for r in Resampling
-                        if r.name in ['nearest', 'cubic', 'average', 'mode', 'gauss']}
 
 GTIFF_OVR_DEFAULTS = {'resampling': 'average',
                       'factors': [2, 4, 8, 16, 32],
                       'internal_storage': True}
-
