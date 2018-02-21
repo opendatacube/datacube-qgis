edd76e9e1dea8a73b4bfc0efbddce12940fb6cc1
Luke Pinner <lpinner@users.noreply.github.com>
2018-02-22 09:17:25 +1100

    More tests

--- src/datacube_query/test/test_utils.py
+++ src/datacube_query/test/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest.mock import patch
-from .utils import data_path, shut_gdal_up
+from .helpers import data_path, shut_gdal_up
 
 import tempfile
 import shutil
@@ -8,11 +8,10 @@ from pathlib import Path
 
 import datacube
 import numpy as np
-from osgeo import gdal
+import pandas as pd
 import rasterio as rio
 import xarray
 
-from datacube_query.exceptions import NoDataError
 import datacube_query.utils
 
 
@@ -48,12 +47,67 @@ def test_datetime_to_str():
     assert datacube_query.utils.datetime_to_str(dt64) == '2001-12-31'
     assert datacube_query.utils.datetime_to_str(dt64, '%Y-%m-%d_%H-%M-%S') == '2001-12-31_01-23-45'
     assert datacube_query.utils.datetime_to_str(dt64, '%Y:%m:%d %H:%M:%S') == '2001:12:31 01:23:45'
+    with pytest.raises(OverflowError):
+        bad_dt64 = np.datetime64('1969-12-31T11:59:59')  # Must be >= 1970
+        datacube_query.utils.datetime_to_str(bad_dt64)
+
+
+@patch('datacube.Datacube')
+def test_get_products_and_measurements(mock_datacube):
+    from datacube.utils.geometry import CRS
+
+    products = {
+        'id': 42,
+        'name': {42: 'some_dataset'}, 'description': {42: 'Some Dataset'}, 'format': {42: 'NETCDF'},
+        'orbit': {42: np.nan}, 'time': {42: None}, 'lat': {42: np.nan}, 'platform': {42: 'HHG2U'},
+        'instrument': {42: 'a towel'}, 'product_type': {42: 'stuff'}, 'crs': {42: CRS('EPSG:4326')},
+        'resolution': {42: [-0.05, 0.05]}, 'tile_size': {42: None},
+        'spatial_dimensions': {42: ('latitude', 'longitude')}}
+
+    df_products = pd.DataFrame(products)
+    df_products.set_index(['id'])
+
+    measurements = {
+        'product': 'some_dataset', 'measurement': 'some_data',
+        'dtype': {('some_dataset', 'some_data'): 'float32'},
+        'name': {('some_dataset', 'some_data'): 'some_data'},
+        'nodata': {('some_dataset', 'some_data'): -999},
+        'spectral_definition': {('some_dataset', 'some_data'): np.nan},
+        'units': {('some_dataset', 'some_data'): 'fm'}}
+
+    expected_with_aliases = {'Some Dataset (some_dataset)': {
+        'product': 'some_dataset',
+        'measurements': {'some_data (foo/bar)': 'some_data'}}}
+    expected_without_aliases = {'Some Dataset (some_dataset)': {
+        'product': 'some_dataset',
+        'measurements': {'some_data': 'some_data'}}}
+
+    measurements_with_aliases = measurements.copy()
+    measurements_with_aliases['aliases'] = {('some_dataset', 'some_data'): ['foo','bar']}
+    df_measurements_with_aliases = pd.DataFrame(measurements_with_aliases)
+    df_measurements_with_aliases.set_index(['product','measurement'])
+
+    measurements_without_aliases = measurements.copy()
+    measurements_without_aliases['aliases'] = {('some_dataset', 'some_data'): np.nan}
+    df_measurements_without_aliases = pd.DataFrame(measurements_without_aliases)
+    df_measurements_without_aliases.set_index(['product','measurement'])
+
+    mock_datacube().list_products.return_value = pd.DataFrame(df_products)
+
+    mock_datacube().list_measurements.return_value = pd.DataFrame(df_measurements_with_aliases)
+    test_with_aliases = datacube_query.utils.get_products_and_measurements()
+    assert test_with_aliases == expected_with_aliases
+
+    mock_datacube().list_measurements.return_value = pd.DataFrame(df_measurements_without_aliases)
+    test_without_aliases = datacube_query.utils.get_products_and_measurements()
+    assert test_without_aliases == expected_without_aliases
 
 
 @patch('datacube.Datacube')
 def test_run_query_no_datasets(mock_datacube):
+    from datacube_query.exceptions import NoDataError
 
-    mock_datacube().index.datasets.search_eager.return_value =  None
+    mock_datacube().index.datasets.search_eager.return_value = None
 
     query = {'product': 'tma', 'measurements': ['1', '4', '9'],
              'x': (19680402.0, 19680205.0), 'y': (-19680205.0, -19680402.0),
@@ -65,6 +119,7 @@ def test_run_query_no_datasets(mock_datacube):
 
 @patch('datacube.Datacube')
 def test_run_query_no_data(mock_datacube):
+    from datacube_query.exceptions import NoDataError
 
     mock_datacube().load.return_value = xarray.Dataset({})
 
